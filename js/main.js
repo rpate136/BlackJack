758eff72f508b07205a1c35125dbfd12eee8a827
rpate136 <rishiapatel111@gmail.com>
2022-05-01 19:10:32 -0400

    Co-authored-by: Hearsch-Jariwala <Hearsch-Jariwala@users.noreply.github.com>

---
+++ js/main.js
@@ -0,0 +1,19 @@
+function cardValue(){
+    
+    console.log("Hello")    
+    value = 'card rank-9 hearts'
+    currCard = document.getElementById("card1").innerHTML
+    console.log(currCard)
+    return value
+  
+}
+
+function cardValue2(){
+    
+    console.log("Hello")    
+    value = 'card rank-9 hearts'
+    currCard = document.getElementById("card1").innerHTML
+    console.log(currCard)
+    return value
+  
+}
